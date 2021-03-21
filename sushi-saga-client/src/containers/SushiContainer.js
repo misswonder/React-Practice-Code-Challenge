30d30b1a26d5f0d1a7fdce2b6a8d2952c81a1db8
Yvonne Chen <yvonnechen1991@gmail.com>
2021-03-21 00:34:50 -0500

    commit tree 9811840fb3396a0f06008d35283b67b9c7576ab2

--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,19 +1,42 @@
-import React, { Fragment } from 'react'
-import MoreButton from '../components/MoreButton'
+import React, { Fragment } from "react";
+import MoreButton from "../components/MoreButton";
+import Sushi from "../components/Sushi";
 
-const SushiContainer = (props) => {
-  return (
-    <Fragment>
-      <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
-      </div>
-    </Fragment>
-  )
+class SushiContainer extends React.Component {
+  state = {
+    sushis: [],
+    startIndex: 0
+  };
+
+  componentDidMount = async () => {
+    const res = await fetch("http://localhost:3000/sushis")
+    const sushis = await res.json()
+    this.setState({ sushis })
+  }
+
+  filteredSushi = () => this.state.sushis.slice(this.state.startIndex, this.state.startIndex + 4)
+
+  moreSushis = () => {
+    let startIndex = this.state.startIndex + 4;
+
+    if (startIndex >= this.state.sushis.length) {
+      startIndex = 0;
+    }
+
+    this.setState({ startIndex });
+  }
+
+  render() {
+
+    return (
+      <Fragment>
+        <div className="belt">
+          {this.filteredSushi().map(sushi => <Sushi onClick={() => this.props.eatSushis(sushi)} sushi={sushi} key={sushi.id}/>)}
+          <MoreButton onClick={this.moreSushis} />
+        </div>
+      </Fragment>
+    );
+  }
 }
 
-export default SushiContainer
+export default SushiContainer;
