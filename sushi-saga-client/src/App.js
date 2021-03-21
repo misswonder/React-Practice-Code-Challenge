8d611ef8804f40899acb8d225af59b8805d00a3c
Yvonne Chen <yvonnechen1991@gmail.com>
2021-03-21 00:34:53 -0500

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,11 +7,43 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    balance: 100,
+    eatenSushis: []
+  }
+
+  eatSushis = (sushi) => {
+    if (this.state.balance < sushi.price || sushi.eaten) {
+      return;
+    }
+
+    sushi.eaten = true 
+
+    this.setState({
+      eatenSushis: [...this.state.eatenSushis, sushi],
+      balance: this.state.balance - sushi.price 
+    })
+  }
+
+  addBalance = (e) => {
+    e.preventDefault();
+
+    const balance = parseInt(e.target.balance.value);
+    this.setState({ balance: this.state.balance + balance });
+
+    e.target.reset();
+  }
+
+
   render() {
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer balance={this.state.balance} eatSushis={this.eatSushis}/>
+        <Table balance={this.state.balance} eatenSushis={this.state.eatenSushis}/>
+        <form onSubmit={this.addBalance}>
+          <input type="number" name="balance" />
+          <button type="submit">Add Balance</button>
+        </form>
       </div>
     );
   }
